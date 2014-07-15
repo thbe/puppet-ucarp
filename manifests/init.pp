fe05b994131ba8738ba8c1518ab55fbc39689cb7
John Miller <johnmill@brandeis.edu>
2014-07-15 13:28:43 -0400

    Reverting changes back to upstream versions to avoid merge problems.

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,25 +18,15 @@
 # [*vPw*]
 #   Set the ucarp password
 #
-# [*upscript*]
-#   Set a template for the "upscript," or script that runs when your vIp comes online.
-#   Defaults to Red Hat-provided script.
-#
-# [*downscript*]
-#   Set a template for the "downscript," or script that runs when your vIp goes offline.
-#   Defaults to Red Hat-provided script.
-#
 # === Variables
 #
 # === Examples
 #
 #  class { '::ucarp':
-#    vId        => '001',
-#    vIp        => '192.168.0.1',
-#    vIf        => 'eth0',
-#    vPw        => 'SuperHyperSecret',
-#    upscript   => 'mymodule/vip-up.erb',
-#    downscript => 'mymodule/vip-down.erb',
+#    vId => '001',
+#    vIp => '192.168.0.1',
+#    vIf => 'eth0',
+#    vPw => 'SuperHyperSecret'
 #  }
 #
 # === Authors
@@ -51,15 +41,9 @@ class ucarp (
   $vId = $ucarp::params::vId,
   $vIp = $ucarp::params::vIp,
   $vIf = $ucarp::params::vIf,
-  $vPw = $ucarp::params::vPw,
-  $upscript = $ucarp::params::upscript,
-  $downscript = $ucarp::params::downscript) inherits ucarp::params {
-
+  $vPw = $ucarp::params::vPw)  inherits ucarp::params {
   # Require class yum to have the relevant repositories in place
   require yum
-
-  # Include Puppetlabs standard library
-  include stdlib
 
   # Start workflow
   if $ucarp::params::linux {
@@ -70,11 +54,5 @@ class ucarp (
     Class['ucarp::package'] ->
     Class['ucarp::config'] ->
     Class['ucarp::service']
-    anchor { 'ucarp::start': }
-    -> class { 'ucarp::package': }
-    ~> class { 'ucarp::config': }
-    ~> class { 'ucarp::scripts': }
-    ~> class { 'ucarp::service': }
-    ~> anchor { 'ucarp::end': }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -25,9 +25,6 @@ class ucarp::params {
 
       # Service definition
       $serviceName = 'ucarp'
-
-      # Upscript/Downscript directory
-      $scriptDir = '/usr/libexec/ucarp'
     }
     default  : {
       $linux = false
@@ -39,6 +36,4 @@ class ucarp::params {
   $vIp = '192.168.0.1'
   $vIf = 'eth0'
   $vPw = 'SuperHyperSecret'
-  $upscript = 'ucarp/vip-up.erb'
-  $downscript = 'ucarp/vip-down.erb'
 }
